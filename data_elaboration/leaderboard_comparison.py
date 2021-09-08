1027954aa06ba27e41a26c64b378b995f47cee48
Filippo Pisello <filippo.pisello@live.it>
2021-09-08 13:08:36 +0200

    refactor: move complete rate_df process in module

--- data_elaboration/leaderboard_comparison.py
+++ data_elaboration/leaderboard_comparison.py
@@ -1,6 +1,29 @@
+from collections import namedtuple
 from statistics import mean
 
+import pandas as pd
+
 from data_elaboration.leaderboard import Leaderboard
+
+
+def rate_df_from_results(
+    df_results: pd.DataFrame, start_season: int, end_season: int, league_label: str
+) -> pd.DataFrame:
+    Rate_Record = namedtuple("Rate", ["Serie", "Season", "Round", "Tolerance", "Rate"])
+
+    rates = []
+    for season in range(start_season, end_season + 1):
+        final_leaderboard = Leaderboard(df_results, season, 38)
+        for round_ in range(1, 38):
+            partial_leaderboard = Leaderboard(df_results, season, round_)
+            for numb in range(0, 3):
+                rate = leaderboard_similarity_rate(
+                    partial_leaderboard, final_leaderboard, tolerance=numb
+                )
+                rates.append(Rate_Record(league_label, season, round_, numb, rate))
+
+    df = pd.DataFrame.from_records(rates, columns=Rate_Record._fields)
+    return df
 
 
 def leaderboard_similarity_rate(
@@ -26,3 +49,8 @@ def leaderboard_similarity_rate(
 
 def are_ranks_similar(rank1: int, rank2: int, tolerance: int) -> bool:
     return abs(rank2 - rank1) <= tolerance
+
+
+def save_rate_df_to_excel(df: pd.DataFrame, league_label: str) -> None:
+    filename = f"saved_dataframes/Similarity Rates_{league_label}.xlsx"
+    df.to_excel(filename, index=False)
